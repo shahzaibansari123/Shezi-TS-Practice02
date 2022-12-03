74cbcbe33271a1ffff48ee3aede4f08748d3d6a5
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-04 00:28:13 +0500

    work complete

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
+// const button = document.querySelector("button")!
+
+// function log(){
+//     console.log("Clicked")
+   
+// }
+
+// button.addEventListener("click",log)
+
+// -----------------------------------------------
+
+
 const button = document.querySelector("button")!
 
-function log(){
+function log(data){
     console.log("Clicked")
    
 }
 
-button.addEventListener("click",log)
+
+log("hello")
