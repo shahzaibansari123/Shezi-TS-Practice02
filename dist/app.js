44f46a6b56a1ccb4c379444d7782611c44e19d52
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-03 23:00:10 +0500

    work complete

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,7 @@
 "use strict";
-//hello typescript
+const button = document.querySelector("button");
+function log() {
+    console.log("Clicked");
+}
+button.addEventListener("click", log);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ const button = document.querySelector("button")!
 
 function log(){
     console.log("Clicked")
+   
 }
 
 button.addEventListener("click",log)
