fd65fa6659e9c96e6faa56688f9800d237ac8e7f
shahzaibansari123 <ansarishahzaib567@gmail.com>
2022-12-13 21:45:09 +0500

    work complete

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,12 @@
 
 // function log(){
 //     console.log("Clicked")
-   
+
 // }
 
 // button.addEventListener("click",log)
 
 // -----------------------------------------------
-
 
 //function binding method
 //...
@@ -16,42 +15,32 @@
 
 // function log(data : string){
 //    console.log("Clicked" + data)
-   
+
 // }
 
 // button.addEventListener('click', log.bind(null, 'pressed')
 //...
 //log("hello")
- 
 
 //.......................................
 
 //Classes
 
 class Student {
-   name: string;
-   rollNo: number;
-   skills: string[]=[];
+  name: string;
+  rollNo: number;
+  skills: string[] = [];
 
+  constructor(n: string, rN: number) {
+    this.name = n;
+    this.rollNo = rN;
+  }
 
-
-
- 
-
-   constructor (n: string , rN: number){
-      this.name = n;
-      this.rollNo= rN;
-      }
-   
-   
-
-
-addSkills(skill: string){
-this.skill.push(skill)
+  addSkills(skill: string) {
+    this.skills.push(skill);
+  }
 }
-   }
 
-
-const student1 = new Student("Shahzaib", 249)
-Student.addSkills("Software")
-console.log(student1)
+const student1 = new Student("Shahzaib", 249);
+student1.addSkills("Software");
+console.log(student1);
